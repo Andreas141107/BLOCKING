317983572448b5aa337a836ef77f21be4e87de99
Andreas141107 <144410195+Andreas141107@users.noreply.github.com>
2023-11-01 14:52:39 +0700

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'logic/changenotifier.dart';
+import 'myhomepage.dart';
+
+void main(){
+  runApp(const MyApp());
+
+}
+class MyApp extends StatelessWidget{
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context){
+    return ChangeNotifierProvider(
+      create:(context) => MyAppState(),
+      child:MaterialApp(debugShowCheckedModeBanner: false,
+      title: 'aplikasi like',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+        home: MyHomePage(),
+    ));
+  }
+}
